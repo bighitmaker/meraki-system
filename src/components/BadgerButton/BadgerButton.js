2dc188deb4c9626abd92a28c8fd928abeda45732
Peter Zenger <peter.zenger1@gmail.com>
2019-01-15 12:35:28 -0800

    babel jiggering

--- src/components/BadgerButton/BadgerButton.js
+++ src/components/BadgerButton/BadgerButton.js
@@ -1,22 +1,20 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 
 const Main = styled.button`
-  border-radius: 8px;
-  color: palevioletred;
-  background-color: mediumaquamarine;
-  padding: 8px 15px;
-  border: none;
-  outline: none;
-`
+	border-radius: 8px;
+	color: palevioletred;
+	background-color: mediumaquamarine;
+	padding: 8px 15px;
+	border: none;
+	outline: none;
+`;
 
 class BadgerButton extends React.Component {
-  render() {
-    const {children, ...rest} = this.props;
-    return (
-      <Main {...rest}>{children}</Main>
-    )
-  }
+	render() {
+		const { children, ...rest } = this.props;
+		return <Main {...rest}>{children}</Main>;
+	}
 }
 
 export default BadgerButton;
